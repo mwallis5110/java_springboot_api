d5f93f2daa92869d5c54f83dac238ca2a19f7cc0
Mason Wallis <mwallis5110@gmail.com>
2022-03-28 10:41:32 -0500

    Added DELETE functionality

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -32,4 +32,14 @@ public class StudentService {
 
         studentRepository.save(student);
     }
+
+    public void deleteStudent(Long studentId) {
+       boolean exists = studentRepository.existsById(studentId);
+       if(!exists) {
+           throw new IllegalStateException(
+               "Student with ID " + studentId + " does not exist"
+           );
+       }
+       studentRepository.deleteById(studentId);
+    }
 }
