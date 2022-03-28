540d72cb05f30979dc7bcca8f6e1a62547d4e265
Mason Wallis <mwallis5110@gmail.com>
2022-03-28 08:19:06 -0500

    Added GET capability

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+@Configuration
+public class StudentConfig {
+    
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mason = new Student(
+                "Mason",
+                "masonwallis@gmail.com",
+                LocalDate.of(2000, Month.JANUARY, 1),
+                22
+            );
+             Student paul = new Student(
+                "Paul",
+                "pauljohnson@gmail.com",
+                LocalDate.of(2001, Month.JANUARY, 7),
+                22
+            );
+
+            repository.saveAll(
+                List.of(mason, paul)
+                );
+        };
+    }
+}
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,22 +3,21 @@ package com.example.demo.student;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDate;
-import java.time.Month;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> 
     getStudents() {
-        return List.of(
-            new Student(
-                1L,
-                "Mason",
-                "masonwallis@gmail.com",
-                LocalDate.of(2000, Month.JANUARY, 1),
-                22
-            )
-        );
+        return studentRepository.findAll();
     }
 }
